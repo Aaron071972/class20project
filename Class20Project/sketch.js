04c2af38e3a263b7c12da4c7c82ff8c373b1cc83
Aaron071972 <64939014+Aaron071972@users.noreply.github.com>
2020-05-28 21:38:26 -0400

    Add files via upload

---
+++ Class20Project/sketch.js
@@ -0,0 +1,42 @@
+var car,wall;
+var speed, weight;
+
+
+
+function setup() {
+speed=random(2,30)
+weight=random(400,1500)
+
+  createCanvas(1600,400);
+  
+  car=createSprite(50, 200, 50, 50);
+  car.velocityX= speed
+
+  wall=createSprite(1500,200,60, height/2)
+
+}
+
+function draw() {
+  background(80,80,80);  
+
+
+  if(wall.x-car.x == wall.width/2 + car.width/2||
+     car.x-wall.x == wall.width/2 + car.width/2||
+
+     wall.y-car.y == wall.height/2 + car.height/2||
+     car.y-wall.y == wall.height/2 + car.height/2
+  ){
+car.shapeColor="Green"
+wall.shapeColor="Red"
+
+car.velocityX=0
+}
+else{
+  car.shapeColor="blue"
+  wall.shapeColor="yellow"
+}
+
+
+
+  drawSprites();
+}
